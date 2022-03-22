669ee0e9c8b5f476c0c53ba5c1d93ca89ca8bd9c
Sean P. Kane <spkane00@gmail.com>
2022-03-22 15:48:50 -0700

    emissary

--- demo-up.sh
+++ demo-up.sh
@@ -15,7 +15,7 @@ export DEMO_ACCOUNT_NUMBER=$(aws sts get-caller-identity --query "Account" --out
 aws eks update-kubeconfig --name "$DEMO_CLUSTER_NAME"
 cd ..
 
-# Helm install Argo CD
+# Install Argo CD
 cd k8s/argocd
 kubectl apply -k overlays/poc
 sleep 120
@@ -23,24 +23,17 @@ argocd login $(kubectl get svc argocd-server -n argocd --no-headers -o=custom-co
 
 # Setup Argo CD to manage itself
 argocd cluster add -y --in-cluster --name local "arn:aws:eks:us-west-2:$DEMO_ACCOUNT_NUMBER:cluster/$DEMO_CLUSTER_NAME"
-argocd repo add https://git.example.com/repos/repo --username git --password secret --tls-client-cert-path ~/mycert.crt --tls-client-cert-key-path ~/mycert.key
-
-
-argocd app create argocd --repo https://argoproj.github.io/argo-helm --helm-chart argo-cd --revision 4.2.0 --dest-namespace argocd --dest-server https://kubernetes.default.svc --sync-policy none
+argocd repo add https://github.com/spkane/poc-terraform-eks-argocd --name poc
+argocd app create argocd --repo https://github.com/spkane/poc-terraform-eks-argocd --path k8s/argocd/overlays/poc --dest-namespace argocd --dest-server https://kubernetes.default.svc
 argocd app sync argocd --async
-kubectl patch cm argocd-cm -n argocd -p '{"data": {"resource.compareoptions": "ignoreAggregatedRoles: true\n"}}'
-
-# Since we are not using our own repo for Argo CD in this demo, we lost some of our configs with the manual sync
-# Let's re-apply then and then relogin via the argocd CLI
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-sleep 120
-argocd login $(kubectl get svc argocd-server -n argocd --no-headers -o=custom-columns=LB:.status.loadBalancer.ingress[0].hostname) --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --insecure
+cd ../..
 
 # Let's install emissary-ingress
 # CRDs will be via kubectl for now
 # The rest of the app will be via Argo CD & helm
-kubectl apply -f https://app.getambassador.io/yaml/emissary/2.2.2/emissary-crds.yaml
-argocd app create emissary-ingress --repo https://app.getambassador.io --helm-chart emissary-ingress --revision 7.3.2 --dest-namespace emissary --dest-server https://kubernetes.default.svc --sync-policy automated --sync-option CreateNamespace=true
+cd k8s/emissary-ingress
+argocd app create emissary-ingress --repo https://github.com/spkane/poc-terraform-eks-argocd --path k8s/emissary-ingress/overlays/poc --dest-namespace emissary-ingress --dest-server https://kubernetes.default.svc
+cd ../..
 
 # Let's open up the Argo CD web UI
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
