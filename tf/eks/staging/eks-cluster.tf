8a3bbdfaeab8df9d65e66f856cbf981cf0d6fc80
Sean P. Kane <spkane00@gmail.com>
2022-03-24 09:47:51 -0700

    upgrade test

--- tf/eks/staging/eks-cluster.tf
+++ tf/eks/staging/eks-cluster.tf
@@ -7,6 +7,6 @@ module "cluster" {
   cluster_version    = var.cluster_version
   vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
   private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
-  public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  public_subnet_ids  = data.terraform_remote_state.vpc.outputs.public_subnet_ids
   tags               = var.tags
 }
